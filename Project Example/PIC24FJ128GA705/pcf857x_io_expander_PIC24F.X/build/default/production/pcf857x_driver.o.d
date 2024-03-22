9e685714db6a1e1d434c6306db1874745ea40ee7
LibraryMaster <132894181+LibraryMasters@users.noreply.github.com>
2024-03-22 08:49:13 +0200

    Delete Project Example/PIC24FJ128GA705/pcf857x_io_expander_PIC24F.X directory

--- Project Example/PIC24FJ128GA705/pcf857x_io_expander_PIC24F.X/build/default/production/pcf857x_driver.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/pcf857x_driver.o: pcf857x_driver.c \
- pcf857x_driver.h
-
-pcf857x_driver.h:
--- Project Example/PIC24FJ128GA705/pcf857x_io_expander_PIC24F.X/build/default/production/pcf857x_driver_basic.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/pcf857x_driver_basic.o: pcf857x_driver_basic.c \
- pcf857x_driver_basic.h pcf857x_driver_interface.h pcf857x_driver.h
-
-pcf857x_driver_basic.h:
-
-pcf857x_driver_interface.h:
-
-pcf857x_driver.h:
